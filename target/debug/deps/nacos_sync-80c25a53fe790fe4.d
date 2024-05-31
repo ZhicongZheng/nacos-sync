66a6fb633e6165f301dc8729172d5471ca6064f4
zhicong <zhicong.zheng@intelliprogroup.com>
2024-05-31 14:44:22 +0800

    init

---
+++ target/debug/deps/nacos_sync-80c25a53fe790fe4.d
@@ -0,0 +1,5 @@
+/Users/zhicong/RustroverProjects/nacos-sync/target/debug/deps/libnacos_sync-80c25a53fe790fe4.rmeta: src/main.rs
+
+/Users/zhicong/RustroverProjects/nacos-sync/target/debug/deps/nacos_sync-80c25a53fe790fe4.d: src/main.rs
+
+src/main.rs:
